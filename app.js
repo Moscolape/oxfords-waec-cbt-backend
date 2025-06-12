f9eb703310fc6f8c6b41a1e3d5a7e51212d142e7
Moscolape <chunkums088@gmail.com>
2025-06-12 14:13:38 +0100

    chore: fix cors error again

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
-  "https://oxfordswaeccbt.netlify.app/"
+  "https://oxfordswaeccbt.netlify.app"
 ];
 
 app.use(
